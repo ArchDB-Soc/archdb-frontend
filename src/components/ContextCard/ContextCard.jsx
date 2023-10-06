4aaa67e1cb1615abb477bc4f97aaa71833e62e0c
Penelope <freyalabram@gmail.com>
2023-10-06 12:05:52 +0200

    add extra fields to contextCard

--- src/components/ContextCard/ContextCard.jsx
+++ src/components/ContextCard/ContextCard.jsx
@@ -5,6 +5,9 @@ const ContextCard = ({ context }) => {
     <div className="card">
       <h4>ID: {context._id}</h4>
       <p>{context.description}</p>
+      <p>{context.eastings}</p>
+      <p>{context.checkedBy}</p>
+      <p>{context.notes}</p>
     </div>
   )
 }
