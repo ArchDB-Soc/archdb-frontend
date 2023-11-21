fa181c83a5e4eb76ba8a7e798b485c2538271819
Penelope <freyalabram@gmail.com>
2023-11-21 12:27:00 +0100

    refactor all entities calls except PUT

--- src/api/entities.jsx
+++ src/api/entities.jsx
@@ -1,67 +1,92 @@
 import { apiURL } from "../const/urls";
 const userStored = JSON.parse(localStorage.getItem('userStored'))
 
-const simpleRequest = async (url, method) => {
+const simpleRequest = async (url, method, requestBody) => { // requestBody is optional
+  const headers = (method === "GET") ? {} : {
+    Authorization: `Bearer ${userStored?.token}`,
+    "Content-Type": "application/json",
+  } // currently all requests except GET require authorisation
+
   try {
     const data = await fetch(url, {
       method: method,
-      headers: {
-      },
+      headers: headers,
+      body: requestBody ? requestBody : null,
     });
     const dataToJson = await data.json();
-    console.log("data retrieved")
+    console.log(`API request with method ${method} successful`)
     return dataToJson
   } catch (error) {
     console.log("error:", error);
   }
 }
 
-
 export const getAllDataFromDb = async (setData, type) => {
   const url = `${apiURL}/${type}`
   const method = "GET"
-  const data = await simpleRequest(url, method)
-  setData(data.data)
-
-  // try {
-  //   const data = await fetch(`${apiURL}/${type}`, {
-  //     method: "GET",
-  //     headers: {
-  //     },
-  //   });
-  //   const dataToJson = await data.json();
-  //   setData(dataToJson.data);
-  // } catch (error) {
-  //   console.log("error:", error);
-  // }
+  const response = await simpleRequest(url, method)
+  setData(response.data)
 };
 
 export const getPaginatedDataFromDb = async (setData, type, page) => {
-
-  try {
-    const url = page ? `${apiURL}/${type}?page=${page}` : `${apiURL}/${type}`
-    const data = await fetch(url, {
-      method: "GET",
-      headers: {
-      },
-    });
-    const dataToJson = await data.json();
-
-    setData(prevData => {
-      if ((Object.keys(prevData[0]).length === 0) && !prevData[1]) {
-        return dataToJson.data;
-      } else {
-        return [...prevData, ...dataToJson.data];
-      }
-    });
-
-    console.log(`Got data from page ${page ? page : 1} from the API`)
-  } catch (error) {
-    console.log("error:", error);
-  }
+  const url = page ? `${apiURL}/${type}?page=${page}` : `${apiURL}/${type}`
+  const method = "GET"
+  const response = await simpleRequest(url, method)
+  setData(prevData => {
+    if ((Object.keys(prevData[0]).length === 0) && !prevData[1]) {
+      return response.data;
+    } else {
+      return [...prevData, ...response.data];
+    }
+  });
+  console.log(`Page ${page ? page : 1} retrieved`)
 };
 
+export const deleteDataFromDb = async (type, id, parentid) => { //parentid is optional and not available for Sites
+  const urlCreator = (type, id, parentid) => {
+    switch (type) {
+      case "sites":
+        return `${apiURL}/sites/${id}`;
+      case "records":
+        return `${apiURL}/sites/${parentid}/records/${id}`;
+      case "sets":
+        return `${apiURL}/sites/${parentid}/sets/${id}`;
+      default:
+        console.log("no element found with that id or parent id");
+        return `${apiURL}/404`;
+    }
+  }
+  const url = urlCreator(type, id, parentid)
+  const method = "DELETE"
+  await simpleRequest(url, method)
+  location.reload()
+};
+
+export const addDataToDb = async (data, type) => {
+  const urlCreator = (type) => {
+    switch (type) {
+      case "sites":
+        return `${apiURL}/sites`;
+      default:
+        console.log("no element found with that id or parent id");
+        return `${apiURL}/404`;
+    }
+  }
+  const url = urlCreator(type)
+  const method = "POST"
+  const body = JSON.stringify(data)
+  await simpleRequest(url, method, body)
+};
+
+// PUT requests
 export const updateDataInDb = async (dataToUpdate, type) => {
+  // PREPARING TO ADD THIS CODE BACK IN AFTER PUT REQUEST BUG IS FIXED
+  // const url = `${apiURL}/${type}/${dataToUpdate._id}`
+  // const method = "PUT"
+  // const body = JSON.stringify(dataToUpdate)
+  // const response = await simpleRequest(url, method, body)
+  // console.log("Record updated:", JSON.stringify(response));
+
   try {
     const response = await fetch(`${apiURL}/${type}/${dataToUpdate._id}`, {
       method: "PUT",
@@ -83,64 +108,13 @@ export const updateDataInDb = async (dataToUpdate, type) => {
   location.reload()
 };
 
-export const deleteDataFromDb = async (type, id, parentid) => { //parentid is optional and not available for Sites
-  const urlCreator = (type, id, parentid) => {
-    switch (type) {
-      case "sites":
-        return `${apiURL}/sites/${id}`;
-      case "records":
-        return `${apiURL}/sites/${parentid}/records/${id}`;
-      case "sets":
-        return `${apiURL}/sites/${parentid}/sets/${id}`;
-      default:
-        console.log("no element found with that id or parent id");
-        return `${apiURL}/404`;
-    }
-  }
-  const url = urlCreator(type, id, parentid)
-  console.log(url)
-  try {
-    await fetch(`${url}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${userStored?.token}`,
-        "Content-Type": "application/json",
-      },
-    })
-
-    location.reload()
-  } catch (error) {
-    console.error("Error:", error);
-  }
-};
-
-export const addSiteToDb = (data) => {
-  fetch(`${apiURL}/sites`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${userStored?.token}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      console.log("Saved:", JSON.stringify(response));
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
-
 export const addXToY = async (x, y, updatedData, yId) => {
 
-  // addXToY("set", "site", data, data._site)
-
   const urlBuilder = (baseUrl, x, y, yId) => `${baseUrl}/${y}s/${yId}/${x}s`
-  const dbUrl = urlBuilder(apiURL, x, y, yId)
+  const url = urlBuilder(apiURL, x, y, yId)
 
   try {
-    const response = await fetch(dbUrl, {
+    const response = await fetch(url, {
       method: "PUT",
       headers: {
         Authorization: `Bearer ${userStored?.token}`,
